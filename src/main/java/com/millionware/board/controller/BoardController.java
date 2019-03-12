19128a618fd5aeb526318dac907ac036685ab33e
restnfeel <restnfeel@gmail.com>
2019-03-12 21:36:12 +0900

    1. swagger add

---
+++ src/main/java/com/millionware/board/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.millionware.board.controller;
+
+import com.millionware.board.entity.Board;
+import com.millionware.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/board")
+public class BoardController {
+
+    @Autowired
+    private BoardRepository repo;
+
+    @GetMapping
+    public ResponseEntity<List<Board>> getBoardAll() {
+
+        List<Board> list = repo.findAll();
+
+        return new ResponseEntity<List<Board>>(list, HttpStatus.OK);
+    }
+
+}
